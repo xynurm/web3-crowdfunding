06d7999238c1297416997d0831b6267db2d3db9a
xynurm <nyakridha24@gmail.com>
2023-11-30 22:58:22 +0700

    add function createCampaign

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -17,4 +17,33 @@ contract CrowdFunding {
     mapping(uint256 => Campaign) public campaigns;
 
     uint256 public numberOfCampaigns = 0;
+
+    
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(
+            campaign.deadline < block.timestamp,
+            "The deadline should be a date in the future."
+        );
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.description = _description;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
 }
