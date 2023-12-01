6e5000da3e7e6b2504a17501c7605937dbdd20e4
xynurm <nyakridha24@gmail.com>
2023-12-01 09:20:45 +0700

    add getDonators

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -62,5 +62,8 @@ contract CrowdFunding {
         }
     }
 
-   
+    function getDonators(uint256 _id)view public returns (address[]memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
 }
