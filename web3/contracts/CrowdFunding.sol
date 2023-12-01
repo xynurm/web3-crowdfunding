66498ca1f13db83e6320db62486bdb65fc7eca6e
xynurm <nyakridha24@gmail.com>
2023-12-01 09:20:23 +0700

    add donateToCampaign

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -46,4 +46,21 @@ contract CrowdFunding {
 
         return numberOfCampaigns - 1;
     }
+
+    function donateToCompaign(uint256 _id) public payable{
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+        
+        if(sent){
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+   
 }
